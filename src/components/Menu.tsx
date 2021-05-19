6055d26b8c2029b91cf9743cadfbb713d29f80ff
FelipeAguiar1 <felipeedu1516@gmail.com>
2021-05-19 18:50:47 -0300

    some init msg

---
+++ src/components/Menu.tsx
@@ -0,0 +1,24 @@
+import styles from "../components_css/Menu.module.css"
+
+export function Menu() {
+    return (
+        <div className={styles.menu}>
+            <header>
+                <div>
+                    <p>DevFULL</p>
+                </div>
+                <p>Utilize o melhor</p>
+            </header>
+            <footer>
+                <div>
+                    <p><a href="#">Principal</a></p>
+                    <p><a href="#">Blog</a></p>
+                    <p><a href="#">Sobre</a></p>
+                    <p><a href="#">Contatos</a></p>
+                </div>
+            </footer>
+            <div style={{width: '85%'}}/>
+            <input type="search" placeholder="    Pesquisar"/>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Menu } from "../components/Menu";
+
+
+export default function Home() {
+  return (
+    <div>
+      <section>
+        <Menu />
+      </section>
+    </div>
+  )
+}
